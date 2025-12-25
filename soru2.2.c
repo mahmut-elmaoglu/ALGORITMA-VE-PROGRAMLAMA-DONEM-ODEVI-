9d2febc5ff55567214e1d9d36abfb99ef96a6e7f
mahmut-elmaoglu <mahmutt.2.7.63.27@gmail.com>
2025-12-25 16:05:20 +0300

    Refactor comments in triangle printing program
    Updated comments for clarity and consistency.

--- soru2.2.c
+++ soru2.2.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 
-// Alıştırma 2.2
+//Alıştırma 2.2
 int main() {
     int n, i, j, k;
 
     printf("Ucgenin yuksekligi (n) kac olsun: ");
     scanf("%d", &n);
 
-    // Dış döngü: Satırları (1'den n'e kadar) kontrol eder
+    //Satırları 1'den n'ye kadar kontrol etme
     for (i = 1; i <= n; i++) {
         
-        // 1. İç Döngü: Eşkenar görünümü için baştaki boşlukları basar
-        // Her satırda (n - i) kadar boşluk bırakırız.
+        //Eşkenar görünümü için baştaki boşlukları basma
+        //Her satırda n - i kadar boşluk bırakıyoruz
         for (j = 1; j <= n - i; j++) {
             printf(" "); 
         }
 
-        // 2. İç Döngü: Sayıları basar
-        // Her satırda o satırın numarasına kadar (1'den i'ye) sayı basarız.
+        //Sayıları basma
+        //Her satırda o satırın numarasına kadar 1'den i'ye sayı basarız.
         for (k = 1; k <= i; k++) {
-            // Sayı ve yanında bir boşluk basarak üçgeni genişletiyoruz
+            //Sayı ve yanında bir boşluk basarak üçgeni genişletiyoruz
             printf("%d ", k);
         }
 
-        // Satır bittiğinde alt satıra geç
+        
         printf("\n");
     }
 
     return 0;
+
 }
